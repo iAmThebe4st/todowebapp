5725ba80bbe6e1bf304880d3bdbd53edebc22310
Vikramaditya Dhumal <dhumalvikramaditya287@gmail.com>
2023-08-30 20:48:59 +0530

    Minor fixes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const DATABASE_STRING = "to-do"
 init()
 
-let taskInput = document.getElementById("task-input")
-let addTaskButton = document.getElementById("add-task-button")
+const taskInput = document.getElementById("task-input")
+const addTaskButton = document.getElementById("add-task-button")
 
 addTaskButton.addEventListener("click", onAddButtonClicked)
 
@@ -25,14 +25,20 @@ function onAddButtonClicked() {
   console.log('New task added successfully!')
 }
 
+createTask()
+function createTask() {
+  const tasksContainer = document.getElementById("tasks-container")
+  const p = document.createElement('p')
+}
+
 function init() {
   let tasks = localStorage.getItem(DATABASE_STRING)
   
   if(tasks === null) {
     const emptyArray = []
     localStorage.setItem(DATABASE_STRING, JSON.stringify(emptyArray))
-    console.log("New Database Created!")
+    console.log("New database created!")
   }
-
+  
   delete tasks
 }
