0cb755ee1aa4f37880171aba645fed1896bdbda6
Vikramaditya Dhumal <dhumalvikramaditya287@gmail.com>
2023-08-30 20:03:26 +0530

    Basic UI changes.

---
+++ main.js
@@ -0,0 +1,8 @@
+let taskInput = document.getElementById("task-input")
+let addTaskButton = document.getElementById("add-task-button")
+
+addTaskButton.addEventListener("click", onAddButtonClicked)
+
+function onAddButtonClicked() {
+  
+}
